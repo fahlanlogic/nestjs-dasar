45f6cc38e66e1e57e4c112edcecde597598f0509
ezb.alanfahdi <ezb.alanfahdi@g.com>
2025-01-28 22:56:15 +0700

    http request and http response

---
+++ TUTORIAL.md
@@ -0,0 +1,26 @@
+# Nest CLI
+  | CLI           | Kegunaan |
+  |---------------|----------|
+  | `nest generate module <nama> <path>`     | untuk generate module |
+  | `nest generate controller <nama> <path>` | untuk generate controller |
+<br>
+
+# HTTP Request
+  | Decorator     | Kegunaan |
+  |---------------|----------|
+  | @Req()        | untuk express.Request |
+  | @Param(key?)  | untuk req.params.key? |
+  | @Body(key?)   | untuk req.body.key?   |
+  | @Query(key?)  | untuk req.query.key?  |
+  | @Header(key?) | untuk req.header.key? |
+  | @Ip()         | untuk req.ip          |
+  | @HostParam()  | untuk req.hosts       |
+<br>
+
+# HTTP Response
+  | Decorator             | Kegunaan |
+  |-----------------------|----------|
+  | @HttpCode(value)      | untuk response status code  |
+  | @Header(key, value)   | untuk response header       |
+  | @Redirect(loc, code)  | untuk redirect endpoint     |
+  | @Next()               | untuk express.Nextfunction  |
--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,9 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, HttpRedirectResponse, Query, Redirect } from '@nestjs/common';
 
 @Controller('/api/v1/users')
 export class UserController {
   @Get()
   getUser() {
     return 'Hello user!';
+  }
+
+  // get all Query
+  @Get("/hello")
+  sayHello(
+    @Query() Query: string
+  ): string 
+  {
+    const queryParsing = JSON.parse(JSON.stringify(Query));
+    const { first_name, last_name } = queryParsing;
+    return `Hello ${first_name} ${last_name} 👋`
+  }
+
+  // get spesific Query
+  @Get("/hai")
+  sayHai(
+    @Query("first_name") firstName: string,
+    @Query("last_name") lastName: string
+  ): string 
+  {
+    return `Hai ${firstName} ${lastName} 👋`
+  }
+
+  @Get("/sample-response")
+  @HttpCode(200)
+  @Header("Content-Type", "application/json")
+  sampleResponse() {
+    return {
+      "data" : {
+        "user_name": "johndoe",
+        "first_name": "John",
+        "last_name": "Doe Dong",
+      }
+    }
+  }
+
+  @Get('/redirect')
+  // @Redirect('/api/v1/users/sample-response', 301) // explisit
+  // redirect(): void {}
+
+  // dinamis
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      'url': '/api/v1/users/sample-response',
+      'statusCode': 301
+    }
   }
 }
